abe9ce7f98dd4e301b0a8236b13a501ea03704b5
azunus@dz <a_azouni@outlook.com>
2013-07-18 08:48:00 -0700

    Create PluginFingerprinter.java

---
+++ src/org/zaproxy/zap/extension/cmss/PluginFingerprinter.java
@@ -0,0 +1,16 @@
+package org.zaproxy.zap.extension.cmss;
+
+
+/**
+ *  Enumerate plugins 
+ * @author abdelhadi
+ *
+ */
+public class PluginFingerprinter {
+  
+	public static void pluginEnumerator(String appName){
+		
+	}
+	
+	
+}
