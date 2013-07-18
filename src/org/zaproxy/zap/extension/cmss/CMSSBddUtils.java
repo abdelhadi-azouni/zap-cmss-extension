8733cddca6b87b2a4c046a1950604402c112e633
azunus@dz <a_azouni@outlook.com>
2013-07-18 08:46:32 -0700

    Create CMSSBddUtils.java

---
+++ src/org/zaproxy/zap/extension/cmss/CMSSBddUtils.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.zaproxy.zap.extension.cmss;
+
+import org.python.util.PythonInterpreter;
+
+/**
+ * @author abdelhadi
+ *
+ */
+public class CMSSBddUtils {
+      /* BlindElephant DB */
+  
+	/**
+	 * calls a python script that convert BE db pkl files into xml files 
+	 * @todo implemant a general pkl2xml convertor using python xml serializators
+	 */
+	public static void pkl2xml (){
+		PythonInterpreter interpreter = new PythonInterpreter();
+		interpreter.execfile("test.py"); 
+	}
+}
