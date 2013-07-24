1d449e7b2d71bdd1fd4d5ea7e4700c162b03f727
azunus@dz <a_azouni@outlook.com>
2013-07-24 11:29:01 -0700

    Update PluginFingerprinter.java

--- src/org/zaproxy/zap/extension/cmss/PluginFingerprinter.java
+++ src/org/zaproxy/zap/extension/cmss/PluginFingerprinter.java
@@ -7,8 +7,11 @@ package org.zaproxy.zap.extension.cmss;
  *
  */
 public class PluginFingerprinter {
-  
+	
 	public static void pluginEnumerator(String appName){
+		
+		// technics to extract xml, htaccess, reame ... from components 
+		// and guess the plugin verions using regex on their content
 		
 	}
 	
