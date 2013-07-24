d7d3ddac5b6f193c3398c403c29331465394a360
azunus@dz <a_azouni@outlook.com>
2013-07-24 11:27:33 -0700

    Update Wappalyzer.java

--- src/org/zaproxy/zap/extension/cmss/Wappalyzer.java
+++ src/org/zaproxy/zap/extension/cmss/Wappalyzer.java
@@ -1,227 +1,223 @@
 package org.zaproxy.zap.extension.cmss;
 
-
-import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
+import org.jdom2.Document;
 
-public class Wappalyzer {
-
-    public static void essaie() throws Exception {
-      
-    	
-    	JSONParser parser = new JSONParser();
-    	 
-        try {
-     
-            Object obj = parser.parse(new FileReader("fastGuess/apps.json"));
-     
-            JSONObject jsonObject = (JSONObject) obj;
-     
-            JSONObject apps =  (JSONObject) jsonObject.get("apps");
-            System.out.println(apps.keySet());
-     
-          
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-     
-    	
-    }
-    
-    
-    /**
-     * download apps.json from wappalyzer GIT repo (that file is updated from here) check wappalyzer wiki 
-     * ------>TODO  similar method to update BlinElephant files (this is actually in python)
-     * @throws IOException
-     */
-    public static void bddUpdate() throws IOException{
-		URL website = new URL("https://raw.github.com/ElbertF/Wappalyzer/master/share/apps.json");
+public class PlunginGuesser {
+	
+	
+	/**
+	 * 
+	****** this is will be modular , each app with its own plugin
+	 */
+	
+	// I will start with specific wabaaps, CMSs first
+	// and joomla first
+	public static void joomlaComponentLister(URL url, String componentType){
+		try{
+			InputStream flux = null;
+			if(componentType.compareTo("plugin")==0){
+				flux=new FileInputStream("pluginEnum/joomla_plugins.txt"); 
+			}
+			if(componentType.compareTo("theme")==0){
+				flux=new FileInputStream("pluginEnum/joomla_themes.txt"); 
+			}
+			
+			InputStreamReader lecture=new InputStreamReader(flux);
+			BufferedReader buff=new BufferedReader(lecture);
+			String line;
+			while ((line=buff.readLine())!=null){
+				//System.out.println(line);
+				URL completeUrl = new URL((url.toString()+line).replaceAll(" ", ""));
+				//System.out.println(completeUrl.toString());
+				HttpURLConnection  con = (HttpURLConnection) completeUrl.openConnection();
+				con.setRequestMethod("HEAD");
+				//System.out.println(con.getResponseCode());
+				if(con.getResponseCode() == HttpURLConnection.HTTP_OK){
+					//System.out.println(completeUrl.toString());
+					//System.out.println(con.getResponseCode());
+					System.out.println(componentType+" : "+line+" exists!!");
+					URL rdm = new URL(completeUrl.toString()+"readme.txt");
+					//System.out.println(rdm.toString());
+					HttpURLConnection  conx = (HttpURLConnection) rdm.openConnection();
+					conx.setRequestMethod("HEAD");
+					if(conx.getResponseCode() == HttpURLConnection.HTTP_OK){
+						System.out.println("------------> readme exists !!");
+					}
+				}
+			}
+			buff.close(); 
+		}		
+		catch (Exception e){
+			System.out.println(e.toString());
+		}
+	}
+	
+	
+	
+	public static void prepareJoomlaPluginDB(){
+		//pending
+	}
+	
+	
+	// using fuzzdb from googlecode.com 
+	// j'ai remarqué que la base de wp scan est plus riche avec une difference de format 
+	// celle de fuzz contient le chemin du plugin a partir de l'url : /component/nom_plugin ..
+	// celle de wp scan contirnt que le nom
+	// donc il faut combiner combiner (-_-)
+	
+	// I noted that wpscan files contain better list of plugins 
+	// so TODO: decide if we continue to use fuzzdb or use wpscan files or combine
+	
+	public static void getJoomlaPluginDB() throws IOException{
+		URL website = new URL("https://fuzzdb.googlecode.com/svn/trunk/Discovery/PredictableRes/CMS/joomla_plugins.fuzz.txt");
 	    ReadableByteChannel rbc = Channels.newChannel(website.openStream());
-	    @SuppressWarnings("resource")
-		FileOutputStream fos = new FileOutputStream("fastGuess/apps.json");
+	    @SuppressWarnings("resource") 
+		FileOutputStream fos = new FileOutputStream("pluginEnum/joomla_plugins.txt");
 	    fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-    }
-    
-    /*
-    public static String[] preparePatterns(JSONObject app){
-    	String[] regexes = new String[0];
-    	String[] keys1 = {"url","html","script"}, keys2={"headers","meta"};
-    	for(String key : keys1){
-    		String pattern = (String) app.get(key);
-    		if(pattern != null){
-    			System.out.println(key+" = "+pattern);
-    			regexes = pattern.split("\\\\;");
-    			
-    			System.out.println(regexes[0]);
-    			System.out.println(regexes[1]);
- 
-    			String chaine = "Test regex Java pour <balise1>Wikibooks</balise1> francophone. Java";
-                Pattern p = Pattern.compile("(.*) Java");
-                Matcher m = p.matcher(chaine);
-                while(m.find())
-                        System.out.println(m.group(1)+"  "+m.group(0));
-
-    		}
-    	}
-    	return regexes;
-    }
-    */
-    
-    public static boolean hasApp(JSONObject app, WebPage wp) throws IOException{
-    	Document HTML = wp.getDocument();
-    	String url = wp.getURL().toString();
-    	
-    	
-    	String[] keys1 = {"url","html","script"}, keys2={"headers","meta"};
-    	
-    	for(String key : keys1){
-    		System.out.println(key);
-    		String pattern = (String) app.get(key);
-    		if(pattern != null){
-    			String[] regexes = pattern.split("\\\\;");
-    			for(String regex:regexes){
-    				Pattern p = Pattern.compile(regex);
-    				if(key.compareTo("url")==0){
-    					Matcher m = p.matcher(url);
+	}
+	public static void getJoomlaThemeDB() throws IOException{
+		URL website = new URL("https://fuzzdb.googlecode.com/svn/trunk/Discovery/PredictableRes/CMS/joomla_themes.fuzz.txt");
+	    ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+	    @SuppressWarnings("resource") 
+		FileOutputStream fos = new FileOutputStream("pluginEnum/joomla_themes.txt");
+	    fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+	}
+	
+	
+	
+	
+	
+	//wordpress part
+	public static void wordpressComponentLister(URL url, String componentType){
+		try{
+			InputStream flux = null;
+			if(componentType.compareTo("plugin")==0){
+				flux=new FileInputStream("pluginEnum/wp_plugins.txt"); 
+			}
+			if(componentType.compareTo("theme")==0){
+				flux=new FileInputStream("pluginEnum/wp_themes.txt"); 
+			}
+			
+			InputStreamReader lecture=new InputStreamReader(flux);
+			BufferedReader buff=new BufferedReader(lecture);
+			String line;
+			while ((line=buff.readLine())!=null){
+				//System.out.println(line);
+				URL completeUrl = new URL((url.toString()+line).replaceAll(" ", ""));
+				//System.out.println(completeUrl.toString());
+				HttpURLConnection  con = (HttpURLConnection) completeUrl.openConnection();
+				con.setRequestMethod("HEAD");
+				//System.out.println(con.getResponseCode());
+				if(con.getResponseCode() == HttpURLConnection.HTTP_OK){
+					//System.out.println(completeUrl.toString());
+					//System.out.println(con.getResponseCode());
+					System.out.println(componentType+" : "+line+" exists!!");
+					URL rdm = new URL(completeUrl.toString()+"readme.txt");
+					//System.out.println(rdm.toString());
+					HttpURLConnection  conx = (HttpURLConnection) rdm.openConnection();
+					conx.setRequestMethod("HEAD");
+					if(conx.getResponseCode() == HttpURLConnection.HTTP_OK){
+						System.out.println("------------> readme exists !!");
+						Document doc = (Document) conx.getContent();
+						Pattern p = Pattern.compile("Stable tag: (.+)");
+						Matcher m = p.matcher(doc.toString());
     					while(m.find()){
-    						System.out.println("regex   :   "+regex);
-    						System.out.println(key+"   :   "+m.group(0));
+    						
+    						System.out.println("  version :   "+m.group(0));
     						
     					}
-                            
-    				}
-    				if(key.compareTo("html")==0){
-    					Matcher m = p.matcher(HTML.outerHtml());
-    					while(m.find()){
-    						System.out.println("regex   :   "+regex);
-    						System.out.println(key+"   :   "+m.group(0));
-    					}
-                            
-    				}
-    				if(key.compareTo("script")==0){
-    					for(Element script:wp.getScriptNodes()){
-    						Matcher m = p.matcher(""+script.toString());
-    						while(m.find()){
-    							System.out.println("regex   :   "+regex);
-    							System.out.println(key+"   :   "+m.group(0));
-    						}
-                                
-    					}
-    				}
-    			} 		
-    		}
-    	}
-    	for(String key : keys2){
-    		System.out.println(key);
-    		JSONObject listHorM = (JSONObject) app.get(key);
-    		if(listHorM != null){
-    			
-    			for(Object name:listHorM.keySet()){
-    				String pattern = (String) listHorM.get(name);
-    				String[] regexes = pattern.split("\\\\;");
-    				for(String regex:regexes){
-        				Pattern p = Pattern.compile(regex);
-        				if(key.compareTo("headers")==0){
-        					System.out.println("name == "+name);
-        					if(wp.getHeaders().get(name) != null){
-        							String HeaderContent = wp.getHeaders().get(name).toString(); // <---
-        							Matcher m = p.matcher(HeaderContent);
-                					while(m.find()){
-                						System.out.println("regex   :   "+regex);
-                						System.out.println(key+"   :   "+m.group(0));
-                					}
-                                         
-        					}
-        						
-        				}
-        				if(key.compareTo("meta")==0){
-        					Map<String, String> metaMap = wp.getMetaNodes();
-        					Set<String> names = wp.getMetaNodes().keySet();
-        					for(String metaName : names){
-        						String content = metaMap.get(metaName);
-        						Matcher m = p.matcher(content);
-        						while(m.find()){
-        							System.out.println("regex   :   "+regex);
-        							System.out.println(key+"   :   "+m.group(0));
-        						} 
-        					}
-        				}
-    				}
-    			}  				
-    		}
-    	}
-    	
-    	return true;
-    }
-    
-    public static void analyse(URL url) throws IOException{
-    	ArrayList<String> detectedApps = new ArrayList<String>() ;
-    	WebPage wp = new WebPage(url);
-    	ArrayList<String> applist = new ArrayList<String>() ;
-    	ArrayList<String> categlist = new ArrayList<String>() ;
-    	  
-    	JSONParser parser = new JSONParser();
-   	 
-        try {
-     
-            Object obj = parser.parse(new FileReader("fastGuess/apps.json"));
-     
-            JSONObject jsonObject = (JSONObject) obj;
-     
-            JSONObject apps =  (JSONObject) jsonObject.get("apps");
-            /**
-             * ici ici 
-             */
-            hasApp((JSONObject)apps.get("WordPress"),wp);
-            applist= new ArrayList<String>() ;
-            for (int i = 0 ; i<apps.keySet().size(); i++){
-            	applist.add((apps.keySet().toArray())[i].toString());
-            	//System.out.println(applist.get(i));
-            }
-            
-            JSONObject categories =  (JSONObject) jsonObject.get("categories");
-            categlist = new ArrayList<String>() ;
-            for (int i = 0 ; i<categories.keySet().size(); i++){
-            	categlist.add((categories.keySet().toArray())[i].toString());
-            	//System.out.println(categlist.get(i));
-            } 
-     
-          
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        
-        
-        for(String app:applist){
-        	//if()
-        }
-    	
-    }
-    
-    
-    
+					}
+				}
+			}
+			buff.close(); 
+		}		
+		catch (Exception e){
+			System.out.println(e.toString());
+		}
+	}	
+
+	public static void getWordpressPluginDB() throws IOException{
+		URL website = new URL("https://fuzzdb.googlecode.com/svn/trunk/Discovery/PredictableRes/CMS/wp_plugins.fuzz.txt");
+	    ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+	    @SuppressWarnings("resource") 
+		FileOutputStream fos = new FileOutputStream("pluginEnum/wp_plugins.txt");
+	    fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+	}
+	public static void getWordpressThemeDB() throws IOException{
+		URL website = new URL("https://fuzzdb.googlecode.com/svn/trunk/Discovery/PredictableRes/CMS/wp_themes.fuzz.txt");
+	    ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+	    @SuppressWarnings("resource") 
+		FileOutputStream fos = new FileOutputStream("pluginEnum/wp_themes.txt");
+	    fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+	}
+	
+	// drupal part 
+	public static void drupalComponentLister(URL url, String componentType){
+		try{
+			InputStream flux = null;
+			if(componentType.compareTo("plugin")==0){
+				flux=new FileInputStream("pluginEnum/drupal_plugins.txt"); 
+			}
+			if(componentType.compareTo("theme")==0){
+				flux=new FileInputStream("pluginEnum/drupal_themes.txt"); 
+			}
+			
+			InputStreamReader lecture=new InputStreamReader(flux);
+			BufferedReader buff=new BufferedReader(lecture);
+			String line;
+			while ((line=buff.readLine())!=null){
+				//System.out.println(line);
+				URL completeUrl = new URL((url.toString()+line).replaceAll(" ", ""));
+				//System.out.println(completeUrl.toString());
+				HttpURLConnection  con = (HttpURLConnection) completeUrl.openConnection();
+				con.setRequestMethod("HEAD");
+				//System.out.println(con.getResponseCode());
+				if(con.getResponseCode() == HttpURLConnection.HTTP_OK){
+					//System.out.println(completeUrl.toString());
+					//System.out.println(con.getResponseCode());
+					System.out.println(componentType+" : "+line+" exists!!");
+					URL rdm = new URL(completeUrl.toString()+"readme.txt");
+					//System.out.println(rdm.toString());
+					HttpURLConnection  conx = (HttpURLConnection) rdm.openConnection();
+					conx.setRequestMethod("HEAD");
+					if(conx.getResponseCode() == HttpURLConnection.HTTP_OK){
+						System.out.println("------------> readme exists !!");
+					}
+				}
+			}
+			buff.close(); 
+		}		
+		catch (Exception e){
+			System.out.println(e.toString());
+		}
+	}
+	
+	public static void getDrupalPluginDB() throws IOException{
+		URL website = new URL("https://fuzzdb.googlecode.com/svn/trunk/Discovery/PredictableRes/CMS/drupal_plugins.fuzz.txt");
+	    ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+	    @SuppressWarnings("resource") 
+		FileOutputStream fos = new FileOutputStream("pluginEnum/drupal_plugins.txt");
+	    fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+	}
+	public static void getDrupalThemeDB() throws IOException{
+		URL website = new URL("https://fuzzdb.googlecode.com/svn/trunk/Discovery/PredictableRes/CMS/drupal_themes.fuzz.txt");
+	    ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+	    @SuppressWarnings("resource") 
+		FileOutputStream fos = new FileOutputStream("pluginEnum/drupal_themes.txt");
+	    fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+	}
+	
 }
