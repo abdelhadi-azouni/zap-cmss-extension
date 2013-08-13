a250e975172d01ec5899371191474d64eb03afe2
azunus@dz <a_azouni@outlook.com>
2013-08-13 15:18:49 -0700

    Update CMSSExtension.java

--- src/org/zaproxy/zap/extension/cmss/CMSSExtension.java
+++ src/org/zaproxy/zap/extension/cmss/CMSSExtension.java
@@ -1,8 +1,7 @@
-package org.zaproxy.zap.extension.cmss;
+package org.zaproxy.zap.extension.CMSS;
 
-import java.io.IOException;
 import java.net.URL;
-import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 
 
 public class CMSSExtension {
@@ -14,22 +13,31 @@ public class CMSSExtension {
 	public static void main (String[] args) throws Exception{
 		//CMSSBddUtils.pkl2xml();
 		//CMSFingerprinter.checkUrlContentChecksoms(new URL("https://weblogs.java.net/blog/kohsuke/archive/2007/04/how_to_convert.html"));
+		//WebPage wp = new WebPage(new URL("http://www.joomla.fr/"));
+		
 		//CMSFingerprinter.FingerprintFileBE(new URL("http://www.joomla.fr/"));
-	    
-		
-		
+		/*String hash = CMSFingerprinter.checksum("hadi azounio");
+		System.out.println(hash);*/
+
+
 		//WebAppGuesser.checkIfExist(new URL("http://www.joomla.fr"), "/images/joomla_logo_black.jpg");
 		//WebAppGuesser.setUrlToGuess(new URL("http://www.lemelies.net/"));		
-		WebAppGuesser.guessApps(new URL("http://www.lemelies.net/"));
+		ArrayList<String> list = WebAppGuesser.guessApps(new URL("http://www.joomla.org/"));
+		for(String app:list){
+			System.out.println(":::"+app);
+		}
 		//WebAppGuesser.fingerPrintFile("joomla");
 		
 		
 		//Wappalyzer.essaie();
-	//---->Wappalyzer.analyse(new URL("http://www.brooklynjuggler.com/"));
+		/*ArrayList<String> list = Wappalyzer.analyse(new URL("http://www.joomla.org/"));
+		for(String app:list){
+			System.out.println(":::"+app);
+		}*/
 		//Wappalyzer.bddUpdate();
 		//PlunginGuesser.getWordpressThemeDB();
 		//PlunginGuesser.wordpressComponentLister(new URL("http://cse-club.org/"),"plugin");
-		
+
 	//--->WebPage wp = new WebPage(new URL("https://weblogs.java.net/blog/kohsuke/archive/2007/04/how_to_convert.html"));
 		//System.out.println(""+wp.getHeaders());
 		//System.out.println(""+wp.getDocument());
@@ -37,7 +45,6 @@ public class CMSSExtension {
 		//System.out.println(""+wp.getScriptNodes());
 		//System.out.println(""+wp.getURL());
 		//tt marche hmd
-	
-	   
+
 	}
 }
