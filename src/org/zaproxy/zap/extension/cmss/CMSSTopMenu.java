82f9879f7bbab8bad1981d0107c6f97ba074c56b
azunus@dz <a_azouni@outlook.com>
2013-08-15 10:57:56 -0700

    Update CMSSTopMenu.java

--- src/org/zaproxy/zap/extension/cmss/CMSSTopMenu.java
+++ src/org/zaproxy/zap/extension/cmss/CMSSTopMenu.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and 
  * limitations under the License. 
  */
-package org.zaproxy.zap.extension.CMSS;
+package org.zaproxy.zap.extension.cmss;
 
 import java.net.MalformedURLException;
 import java.net.URL;
