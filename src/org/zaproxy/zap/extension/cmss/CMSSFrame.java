c0f1ff7933c3945c274e6aa9238fdd39954b5e70
azunus@dz <a_azouni@outlook.com>
2013-08-15 10:57:14 -0700

    Update CMSSFrame.java

--- src/org/zaproxy/zap/extension/cmss/CMSSFrame.java
+++ src/org/zaproxy/zap/extension/cmss/CMSSFrame.java
@@ -1,4 +1,4 @@
-package org.zaproxy.zap.extension.CMSS;
+package org.zaproxy.zap.extension.cmss;
 
 
 import java.awt.BorderLayout;
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URL;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
@@ -35,6 +36,7 @@ public class CMSSFrame extends JFrame {
 	private JTextField textField_1;
 	
 	private FingerPrintingThread fpThread = null;
+	private URL targetUrl = null;
 
 	// we save in this list all checkBoses then use them to get web apps category names and then category numbers
 	private ArrayList<javax.swing.JCheckBox> checkBoxesList = new ArrayList<javax.swing.JCheckBox>();
@@ -42,13 +44,16 @@ public class CMSSFrame extends JFrame {
 	private WhatToFingerPrintFrame wtfpFrame = new WhatToFingerPrintFrame();
 	
 	private int POrAOption = 1;
+	private JTextField txtHttp;
 
 	/**
 	 * Create the frame.
 	 */
 	public CMSSFrame() {
+		setTitle("Fingerprinting tools");
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-		setBounds(100, 100, 751, 300);
+		setBounds(100, 100, 756, 372);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
@@ -58,47 +63,34 @@ public class CMSSFrame extends JFrame {
 		contentPane.add(layeredPane, BorderLayout.CENTER);
 		
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.setBounds(0, 0, 725, 251);
+		tabbedPane.setBounds(0, 0, 725, 323);
 		layeredPane.add(tabbedPane);
 		
-		JLayeredPane layeredPane_2 = new JLayeredPane();
-		tabbedPane.addTab("Details", null, layeredPane_2, null);
-		
-		JTabbedPane tabbedPane_1 = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane_1.setBounds(0, 0, 720, 223);
-		layeredPane_2.add(tabbedPane_1);
-		
-		JLayeredPane layeredPane_3 = new JLayeredPane();
-		tabbedPane_1.addTab("Passive fingerprint", null, layeredPane_3, null);
-		
-		JTabbedPane tabbedPane_2 = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane_1.addTab("Agressive fingerprint", null, tabbedPane_2, null);
-		
 		JLayeredPane layeredPane_1 = new JLayeredPane();
-		tabbedPane.addTab("Fingerprinting options", null, layeredPane_1, null);
+		tabbedPane.addTab("Fingerprint", null, layeredPane_1, null);
 		
 		
 		
 		JLabel label = new JLabel("App name:");
-		label.setBounds(28, 118, 76, 14);
+		label.setBounds(35, 188, 76, 14);
 		layeredPane_1.add(label);
 		
 		JLabel label_1 = new JLabel("Version:");
-		label_1.setBounds(28, 160, 76, 14);
+		label_1.setBounds(35, 230, 76, 14);
 		layeredPane_1.add(label_1);
 		
 		textField = new JTextField();
 		textField.setColumns(10);
-		textField.setBounds(114, 118, 109, 29);
+		textField.setBounds(121, 188, 109, 29);
 		layeredPane_1.add(textField);
 		
 		textField_1 = new JTextField();
 		textField_1.setColumns(10);
-		textField_1.setBounds(114, 153, 109, 29);
+		textField_1.setBounds(121, 223, 109, 29);
 		layeredPane_1.add(textField_1);
 		
 		JSeparator separator = new JSeparator();
-		separator.setBounds(35, 84, 168, 2);
+		separator.setBounds(35, 72, 665, 2);
 		layeredPane_1.add(separator);
 		
 		JSeparator separator_1 = new JSeparator();
@@ -107,7 +99,7 @@ public class CMSSFrame extends JFrame {
 		
 		JSeparator separator_2 = new JSeparator();
 		separator_2.setOrientation(SwingConstants.VERTICAL);
-		separator_2.setBounds(253, 11, 1, 201);
+		separator_2.setBounds(260, 81, 1, 201);
 		layeredPane_1.add(separator_2);
 		
 		final JCheckBox chckbxGetVersion = new JCheckBox("Get version");
@@ -118,7 +110,7 @@ public class CMSSFrame extends JFrame {
 				if(!textField_1.isEnabled() && chckbxGetVersion.isSelected()) textField_1.setEnabled(true);
 			}
 		});
-		chckbxGetVersion.setBounds(28, 11, 195, 23);
+		chckbxGetVersion.setBounds(35, 81, 195, 23);
 		layeredPane_1.add(chckbxGetVersion);
 		
 		final JCheckBox chckbxPassiveFingerprinting = new JCheckBox("Passive");
@@ -126,45 +118,45 @@ public class CMSSFrame extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		chckbxPassiveFingerprinting.setBounds(28, 37, 195, 23);
+		chckbxPassiveFingerprinting.setBounds(35, 107, 195, 23);
 		chckbxPassiveFingerprinting.setSelected(true);//
 		layeredPane_1.add(chckbxPassiveFingerprinting);
 		
 		final JCheckBox chckbxAgressive = new JCheckBox("Agressive");
-		chckbxAgressive.setBounds(28, 63, 195, 23);
+		chckbxAgressive.setBounds(35, 133, 195, 23);
 		layeredPane_1.add(chckbxAgressive);
 		
 		JLabel lblWhatToFingerprint = new JLabel("What to fingerprint ?");
-		lblWhatToFingerprint.setBounds(280, 11, 109, 14);
+		lblWhatToFingerprint.setBounds(287, 81, 109, 14);
 		layeredPane_1.add(lblWhatToFingerprint);
 		
 		JCheckBox chckbxCms = new JCheckBox("cms");
-		chckbxCms.setBounds(273, 32, 134, 23);
+		chckbxCms.setBounds(280, 102, 134, 23);
 		layeredPane_1.add(chckbxCms);
 		
 		JCheckBox chckbxMessageboards = new JCheckBox("message-boards");
-		chckbxMessageboards.setBounds(273, 58, 134, 23);
+		chckbxMessageboards.setBounds(280, 128, 134, 23);
 		layeredPane_1.add(chckbxMessageboards);
 		
 		JCheckBox chckbxJavascriptframeworks = new JCheckBox("javascript-frameworks");
-		chckbxJavascriptframeworks.setBounds(274, 84, 133, 23);
+		chckbxJavascriptframeworks.setBounds(281, 154, 133, 23);
 		layeredPane_1.add(chckbxJavascriptframeworks);
 		
 		JCheckBox chckbxWebframeworks = new JCheckBox("web-frameworks");
-		chckbxWebframeworks.setBounds(274, 108, 133, 23);
+		chckbxWebframeworks.setBounds(281, 178, 133, 23);
 		layeredPane_1.add(chckbxWebframeworks);
 		
 		JCheckBox chckbxWebservers = new JCheckBox("web-servers");
-		chckbxWebservers.setBounds(274, 134, 133, 23);
+		chckbxWebservers.setBounds(281, 204, 133, 23);
 		layeredPane_1.add(chckbxWebservers);
 		
 		JSeparator separator_4 = new JSeparator();
 		separator_4.setOrientation(SwingConstants.VERTICAL);
-		separator_4.setBounds(428, 11, 1, 201);
+		separator_4.setBounds(435, 81, 1, 201);
 		layeredPane_1.add(separator_4);
 		
 		JCheckBox chckbxDatabases = new JCheckBox("databases");
-		chckbxDatabases.setBounds(274, 158, 133, 23);
+		chckbxDatabases.setBounds(281, 228, 133, 23);
 		layeredPane_1.add(chckbxDatabases);
 		
 		JButton btnMore = new JButton("More");
@@ -175,11 +167,11 @@ public class CMSSFrame extends JFrame {
 				wtfpFrame.setVisible(true);
 			}
 		});
-		btnMore.setBounds(284, 191, 123, 23);
+		btnMore.setBounds(291, 261, 123, 23);
 		layeredPane_1.add(btnMore);
 		
 		JLabel lblFingerprintingTimeAnd = new JLabel("Fingerprinting time and occuracy settings:");
-		lblFingerprintingTimeAnd.setBounds(483, 11, 210, 14);
+		lblFingerprintingTimeAnd.setBounds(490, 81, 210, 14);
 		layeredPane_1.add(lblFingerprintingTimeAnd);
 		
 		
@@ -192,7 +184,12 @@ public class CMSSFrame extends JFrame {
 				else if(!chckbxPassiveFingerprinting.isSelected() && chckbxAgressive.isSelected()) POrAOption = 2;
 				else if(chckbxPassiveFingerprinting.isSelected() && chckbxAgressive.isSelected()) POrAOption = 3;
 				
-				
+				try {
+					targetUrl = new URL(txtHttp.getText());
+				} catch (MalformedURLException e2) {
+					// TODO Auto-generated catch block
+					e2.printStackTrace();
+				}
 				
 				System.out.println("POrAOption : "+POrAOption);
 				
@@ -204,7 +201,7 @@ public class CMSSFrame extends JFrame {
 				}
 				// we call FastFingerprinter.filterResults on the global whatToFingerPrint List
 					
-				fpThread = new FingerPrintingThread (wtfpList,POrAOption);
+				fpThread = new FingerPrintingThread (targetUrl,wtfpList,POrAOption);
 				fpThread.start();
                 while(fpThread.isAlive()){
                     // waiting;
@@ -219,27 +216,47 @@ public class CMSSFrame extends JFrame {
 				if(chckbxGetVersion.isSelected()){
 					System.out.println("wiw");
 					ArrayList<String> versions = new ArrayList<String>();
+					if(resultList.contains("wordpress")){
+						textField_1.setText(textField_1.getText()+"wordpress :");
+						for(String version: FastFingerprinter.WordpressFastFingerprint(targetUrl)) {
+							textField_1.setText(textField_1.getText()+version+" ; ");
+						}
+					}
+					if(resultList.contains("joomla")){
+						textField_1.setText(textField_1.getText()+"joomla :");
+						for(String version: FastFingerprinter.JoomlaFastFingerprint(targetUrl)) {
+							textField_1.setText(textField_1.getText()+version+" ; ");
+						}
+					}
+					
+					// blindelephant
 					for(String app: resultList){
 						try {
-							versions = WebAppGuesser.fingerPrintFile("Joomla");
+							versions = WebAppGuesser.fingerPrintFile(app);
+							textField_1.setText(textField_1.getText()+app+" : ");
+							for(String version: versions) {
+								textField_1.setText(textField_1.getText()+version+" ; ");
+							}
 						} catch ( NoSuchAlgorithmException | IOException
 								| DecoderException e1) {
 							// TODO Auto-generated catch block
 							e1.printStackTrace();
 						}
 					}
-					for(String version: versions) {
-						textField_1.setText(textField_1.getText()+version+" , ");
-					}
 				}
 			}
 		});
 		
-		btnFingerprint.setBounds(28, 84, 195, 23);
+		btnFingerprint.setBounds(35, 154, 195, 23);
 		layeredPane_1.add(btnFingerprint);
 		
 		JButton btnDetailedView = new JButton("Detailed view ");
-		btnDetailedView.setBounds(28, 189, 195, 23);
+		btnDetailedView.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			    
+			}
+		});
+		btnDetailedView.setBounds(35, 259, 195, 23);
 		layeredPane_1.add(btnDetailedView);
 		this.checkBoxesList.add(chckbxCms);
 		this.checkBoxesList.add(chckbxJavascriptframeworks);
@@ -247,16 +264,41 @@ public class CMSSFrame extends JFrame {
 		this.checkBoxesList.add(chckbxWebservers);
 		this.checkBoxesList.add(chckbxDatabases);
 		this.checkBoxesList.add(chckbxMessageboards);
+		
+		txtHttp = new JTextField();
+		txtHttp.setText("http://");
+		txtHttp.setBounds(128, 22, 568, 29);
+		layeredPane_1.add(txtHttp);
+		txtHttp.setColumns(10);
+		
+		JLabel lblTarget = new JLabel("Target : ");
+		lblTarget.setBounds(51, 29, 46, 14);
+		layeredPane_1.add(lblTarget);
+		
+		JLayeredPane layeredPane_2 = new JLayeredPane();
+		tabbedPane.addTab("Details", null, layeredPane_2, null);
+		
+		JTabbedPane tabbedPane_1 = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane_1.setBounds(0, 0, 720, 223);
+		layeredPane_2.add(tabbedPane_1);
+		
+		JLayeredPane layeredPane_4 = new JLayeredPane();
+		tabbedPane_1.addTab("Detailed result", null, layeredPane_4, null);
+		
+		JLayeredPane layeredPane_3 = new JLayeredPane();
+		tabbedPane_1.addTab("Passive fingerprint", null, layeredPane_3, null);
+		
+		JTabbedPane tabbedPane_2 = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane_1.addTab("Agressive fingerprint", null, tabbedPane_2, null);
 	}
 	private ArrayList<String> getWhatToFingerprint(){
 		ArrayList<String> WhatToFingerprint = new ArrayList<String>();
 		for(JCheckBox checkBox:checkBoxesList){
 			if(checkBox.isSelected()){
-				System.out.println(checkBox.getText());
+				System.out.println("check boxe : "+checkBox.getText());
 				WhatToFingerprint.add(checkBox.getText());
 			}
 		}
 		return WhatToFingerprint;
 	}
-	
 }
