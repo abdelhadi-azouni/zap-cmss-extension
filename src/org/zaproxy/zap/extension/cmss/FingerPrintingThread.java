5cb53a9c45e51e3a7b8283a4c18e74a57f350ac1
azunus@dz <a_azouni@outlook.com>
2013-08-15 10:58:41 -0700

    Update FingerPrintingThread.java

--- src/org/zaproxy/zap/extension/cmss/FingerPrintingThread.java
+++ src/org/zaproxy/zap/extension/cmss/FingerPrintingThread.java
@@ -1,21 +1,24 @@
-package org.zaproxy.zap.extension.CMSS;
+package org.zaproxy.zap.extension.cmss;
 
+import java.net.URL;
 import java.util.ArrayList;
 
 public class FingerPrintingThread extends Thread{
 	private ArrayList<String> wtfpList = new ArrayList<String>();
 	private ArrayList<String> resultList = new ArrayList<String>();
 	private int POrAOption;
+	private URL targetUrl;
     
-	public FingerPrintingThread(ArrayList<String> wtfpList, int POrAOption){
+	public FingerPrintingThread(URL targetUrl, ArrayList<String> wtfpList, int POrAOption){
     		this.wtfpList = wtfpList;
     		this.POrAOption = POrAOption;
+    		this.targetUrl = targetUrl;
 	}
 
 	@Override
 	public void run() {
 		try {
-			resultList = FastFingerprinter.filterResults(wtfpList, this.POrAOption);
+			resultList = FastFingerprinter.filterResults(this.targetUrl, wtfpList, this.POrAOption);
 			this.finalize();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
